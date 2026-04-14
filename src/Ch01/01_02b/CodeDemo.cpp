d305a92a29dd6cd869c98dbc6e7a7217cce2062f
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2026-04-14 00:14:16 +0000

    Commiting more changes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cout << "Hello! What is your name?" << std::flush;
+    std::cin>>str;
+    std::cout<< "Hello " << str << "!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return(0);
+}
