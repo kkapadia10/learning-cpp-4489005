c803a224de77290fc7b82b18be2e9ab7efe13d75
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2026-04-16 00:33:31 +0000

    more changes

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,8 +4,29 @@
 
 #include <iostream>
 
+//#define AGE_LENGTH 4
+
 int main(){
+    const size_t AGE_LENGTH = 4;
+
+    int age[AGE_LENGTH];
+    float temperature[] = {31.5, 32.7, 38.9};
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
     
+    std::cout << "Age array has " << AGE_LENGTH << " elements" << std::endl;
+    std::cout << "age[0] = " << age[0] << std::endl;
+    std::cout << "age[1] = " << age[1] << std::endl;
+    std::cout << "age[2] = " << age[2] << std::endl;
+    std::cout << "age[3] = " << age[3] << std::endl;
+    std::cout << std::endl << std::endl;
+
+    std::cout << "temperature[0] = " << temperature[0] << std::endl;
+    std::cout << "temperature[1] = " << temperature[1] << std::endl;
+    std::cout << "temperature[2] = " << temperature[2] << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,19 @@
 #include <cstdint>
 
 int main(){
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5/9) * (fahrenheit - 32);
+
+    std::cout << "Celsius = " << celsius << std::endl;
+
+    float weight = 10.99;
     
+    std::cout << "float = " << weight << std::endl;
+    std::cout << "int = " << (int)weight << std::endl;
+    std::cout << "fractional part = " << (int)((weight - (int)weight) * 10000) << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
